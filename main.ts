7f82ba03db2f5567109e868f9e8484c5fa950c62
7ijme <tijmevh@gmail.com>
2025-10-24 16:34:12 +0200

    module display :)

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ const socket = new WebSocket("ws://localhost:3000");
 
 import { encodeHex } from "jsr:@std/encoding/hex";
 import {
-  CanvasRenderingContext2D,
   createCanvas,
   EmulatedCanvas2D,
 } from "https://deno.land/x/canvas/mod.ts";
@@ -13,8 +12,8 @@ socket.addEventListener("open", () => {
   setInterval(() => {
     // get time
     const now = new Date();
-    const hours = now.getMinutes();
-    const minutes = now.getSeconds();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
 
     const nums = [
       Math.floor(hours / 10),
@@ -27,30 +26,16 @@ socket.addEventListener("open", () => {
         command: "send_png",
         params: [
           `path_or_hex=${canvasToHex(
-            combineCanvases(
-              makeSmallClock("br", 0, 0, 0, 0),
-              makeSmallClock("bl", 0, 0, 0, 0),
-            ),
+            combineCanvases({
+			  tl: emoji("🦖"),
+              br: makeSmallClock(...nums),
+              tr: day(),
+            }),
           )}`,
         ],
       }),
     );
   }, 1000);
-  const canvas = createCanvas(64, 16);
-  const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "blue";
-  const fontSize = 8;
-  ctx.font = `${fontSize}px sans-serif`;
-  ctx.fillText("hi", 0, fontSize);
-  const hex = encodeHex(canvas.toBuffer("image/png"));
-  // save to file for debugging
-  Deno.writeFileSync("debug.png", canvas.toBuffer("image/png"));
-  socket.send(
-    JSON.stringify({
-      command: "send_png",
-      params: [`path_or_hex=${hex}`],
-    }),
-  );
 });
 
 socket.addEventListener("message", (event) => {
@@ -88,10 +73,10 @@ function makeClock(...nums: number[]) {
   return hex;
 }
 
-function makeSmallClock(where: "tl" | "tr" | "bl" | "br", ...nums: number[]) {
+function makeSmallClock(...nums: number[]) {
   // Image dimensions
-  const width = 64;
-  const height = 16;
+  const width = 32;
+  const height = 8;
 
   // Create a canvas and fill red
   const canvas = createCanvas(width, height);
@@ -103,26 +88,16 @@ function makeSmallClock(where: "tl" | "tr" | "bl" | "br", ...nums: number[]) {
   const fontSize = 10;
   ctx.font = `${fontSize}px digital-7`;
 
-  const xOffset = where.includes("r") ? 32 : 0;
-  const yOffset = 8 + (where.includes("t") ? 0 : 8);
+  const xOffset = 0;
+  const yOffset = 8;
   ctx.fillText(nums[0].toString(), xOffset, yOffset);
   ctx.fillText(nums[1].toString(), 8 + xOffset, yOffset);
   ctx.fillText(nums[2].toString(), 17 + xOffset, yOffset);
   ctx.fillText(nums[3].toString(), 25 + xOffset, yOffset);
 
   ctx.fillStyle = "blue";
-  ctx.fillRect(
-    where.includes("l") ? 15 : 47,
-    where.includes("t") ? 1 : 10,
-    2,
-    2,
-  );
-  ctx.fillRect(
-    where.includes("l") ? 15 : 47,
-    where.includes("t") ? 5 : 13,
-    2,
-    2,
-  );
+  ctx.fillRect(15, 1, 2, 2);
+  ctx.fillRect(15, 5, 2, 2);
 
   // Get PNG buffer
   // const hex = encodeHex(canvas.toBuffer("image/png"));
@@ -135,11 +110,55 @@ function canvasToHex(canvas: EmulatedCanvas2D) {
   return hex;
 }
 
-function combineCanvases(...canvas: EmulatedCanvas2D[]) {
+type combineCanvas = {
+  tl?: EmulatedCanvas2D;
+  tr?: EmulatedCanvas2D;
+  bl?: EmulatedCanvas2D;
+  br?: EmulatedCanvas2D;
+};
+function combineCanvases(canvases: combineCanvas) {
   const newCanvas = createCanvas(64, 16);
   const ctx = newCanvas.getContext("2d");
-  canvas.forEach((c) => {
-    ctx.drawImage(c, 0, 0);
-  });
+  if (canvases.tl) {
+    ctx.drawImage(canvases.tl, 0, 0);
+  }
+  if (canvases.tr) {
+    ctx.drawImage(canvases.tr, 32, 0);
+  }
+  if (canvases.bl) {
+    ctx.drawImage(canvases.bl, 0, 8);
+  }
+  if (canvases.br) {
+    ctx.drawImage(canvases.br, 32, 8);
+  }
   return newCanvas;
+}
+
+function day() {
+  const canvas = createCanvas(32, 8);
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "green";
+  const fontSize = 8;
+  ctx.font = `${fontSize}px monospace`;
+  const now = new Date();
+  ctx.fillText(now.toDateString().split(" ")?.[0], 0, fontSize - 1);
+  ctx.fillText(
+    now.toDateString().split(" ")?.[2].padStart(2, " "),
+    22,
+    fontSize - 1,
+  );
+  return canvas;
+}
+
+function emoji(emoji: string) {
+  const canvas = createCanvas(32, 16);
+  const ctx = canvas.getContext("2d");
+  canvas.loadFont(Deno.readFileSync("./apple-color-emoji.ttf"), {
+    family: "Apple Color Emoji",
+  });
+  ctx.fillStyle = "green";
+  const fontSize = 14;
+  ctx.font = `${fontSize}px Apple Color Emoji`;
+  ctx.fillText(emoji, 8, fontSize-1);
+  return canvas;
 }
